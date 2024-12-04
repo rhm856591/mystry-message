9d44715d6fd4e542351f9d7e1dce8da09c6bb9f0
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2024-12-04 19:36:48 +0530

    fix

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -11,112 +11,112 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 const page = () => {
-  const [messages, setMessages] = useState<Message[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
+  // const [messages, setMessages] = useState<Message[]>([])
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [isSwitchLoading, setIsSwitchLoading] = useState(false)
 
-  const {toast} = useToast()
+  // const {toast} = useToast()
 
-  const handleDeleteMessage = (messageid: string) =>{
-    setMessages(messages.filter((message)=> message._id !== messageid))
-  }
+  // const handleDeleteMessage = (messageid: string) =>{
+  //   setMessages(messages.filter((message)=> message._id !== messageid))
+  // }
 
-  const {data: session} = useSession()
+  // const {data: session} = useSession()
 
-  const form = useForm({
-    resolver: zodResolver(acceptMessageSchema)
-  })
+  // const form = useForm({
+  //   resolver: zodResolver(acceptMessageSchema)
+  // })
 
-  const {register, watch, setValue} = form;
+  // const {register, watch, setValue} = form;
 
-  const acceptMessages = watch('acceptMessages')
+  // const acceptMessages = watch('acceptMessages')
 
-  const fetchAcceptMessage = useCallback(async ()=>{
-    setIsSwitchLoading(true)
-    try {
-      const response = await axios.get<ApiResponse>('/api/accept-messages')
-      setValue('acceptMessages', response.data.isAcceptingMessage)
+  // const fetchAcceptMessage = useCallback(async ()=>{
+  //   setIsSwitchLoading(true)
+  //   try {
+  //     const response = await axios.get<ApiResponse>('/api/accept-messages')
+  //     setValue('acceptMessages', response.data.isAcceptingMessage)
 
-    } catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>
-      toast({
-        title: "Error",
-        description: axiosError.response?.data.message || "Error while fetching accept messages",
-        variant: "destructive",
-      })
-    } finally {
-      setIsSwitchLoading(false)
-    }
-  },[setValue])
+  //   } catch (error) {
+  //     const axiosError = error as AxiosError<ApiResponse>
+  //     toast({
+  //       title: "Error",
+  //       description: axiosError.response?.data.message || "Error while fetching accept messages",
+  //       variant: "destructive",
+  //     })
+  //   } finally {
+  //     setIsSwitchLoading(false)
+  //   }
+  // },[setValue])
 
-  const fetchMessages = useCallback(async (refresh: boolean = false) =>{
-    setIsLoading(true)
-    setIsSwitchLoading(true)
-    try {
-      const response = await axios.get('/api/accept-message')
-      setMessages(response.data.messages||[])
-      if(refresh) {
-        toast({
-          title: "Refresh Message",
-          description: "Showing latest message",
-        })
-      }
-    } catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>
-      toast({
-        title: "Error",
-        description: axiosError.response?.data.message || "Error while fetching messages",
-        variant: "destructive",
-      })
+  // const fetchMessages = useCallback(async (refresh: boolean = false) =>{
+  //   setIsLoading(true)
+  //   setIsSwitchLoading(true)
+  //   try {
+  //     const response = await axios.get('/api/accept-message')
+  //     setMessages(response.data.messages||[])
+  //     if(refresh) {
+  //       toast({
+  //         title: "Refresh Message",
+  //         description: "Showing latest message",
+  //       })
+  //     }
+  //   } catch (error) {
+  //     const axiosError = error as AxiosError<ApiResponse>
+  //     toast({
+  //       title: "Error",
+  //       description: axiosError.response?.data.message || "Error while fetching messages",
+  //       variant: "destructive",
+  //     })
       
-    } finally{
-      setIsLoading(false)
-      setIsSwitchLoading(false)
-    }
-  },[setIsLoading, setMessages])
+  //   } finally{
+  //     setIsLoading(false)
+  //     setIsSwitchLoading(false)
+  //   }
+  // },[setIsLoading, setMessages])
 
-  useEffect(()=>{
-    if(!session || !session.user) return 
-    fetchMessages()
-    fetchAcceptMessage()
-  },[session, setValue, fetchAcceptMessage,fetchMessages])
+  // useEffect(()=>{
+  //   if(!session || !session.user) return 
+  //   fetchMessages()
+  //   fetchAcceptMessage()
+  // },[session, setValue, fetchAcceptMessage,fetchMessages])
 
-  const handleSwitchChange = async ()=>{
-    setIsSwitchLoading(true)
-    try {
-      const response = await axios.post('/api/accept-message', {acceptMessages:!acceptMessages})
-      setValue('acceptMessages', !acceptMessages)
-      toast({
-        title: "Switch Accept Message",
-        description: `Accept message is now ${response.data.isAcceptingMessage? 'ON' : 'OFF'}`,
-      })
-    } catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>
-      toast({
-        title: "Error",
-        description: axiosError.response?.data.message || "Error while switching accept messages",
-        variant: "destructive",
-      })
-    } finally {
-      setIsSwitchLoading(false)
-    }
-  }
+  // const handleSwitchChange = async ()=>{
+  //   setIsSwitchLoading(true)
+  //   try {
+  //     const response = await axios.post('/api/accept-message', {acceptMessages:!acceptMessages})
+  //     setValue('acceptMessages', !acceptMessages)
+  //     toast({
+  //       title: "Switch Accept Message",
+  //       description: `Accept message is now ${response.data.isAcceptingMessage? 'ON' : 'OFF'}`,
+  //     })
+  //   } catch (error) {
+  //     const axiosError = error as AxiosError<ApiResponse>
+  //     toast({
+  //       title: "Error",
+  //       description: axiosError.response?.data.message || "Error while switching accept messages",
+  //       variant: "destructive",
+  //     })
+  //   } finally {
+  //     setIsSwitchLoading(false)
+  //   }
+  // }
 
-  const {username} = session?.user as User
-  const baseURL = `${window.location.protocol}//${window.location.host}`
-  const profileURL = `${baseURL}/u/${username}`
+  // const {username} = session?.user as User
+  // const baseURL = `${window.location.protocol}//${window.location.host}`
+  // const profileURL = `${baseURL}/u/${username}`
 
-  const copyToClipboard = ()=>{
-    navigator.clipboard.writeText(profileURL)
-    toast({
-      title: "Copy URL",
-      description: "URL copied to clipboard",
-    })
-  }
+  // const copyToClipboard = ()=>{
+  //   navigator.clipboard.writeText(profileURL)
+  //   toast({
+  //     title: "Copy URL",
+  //     description: "URL copied to clipboard",
+  //   })
+  // }
 
-  if(!session || !session.user){
-    return <div>Please login</div>
-  }
+  // if(!session || !session.user){
+  //   return <div>Please login</div>
+  // }
 
 
   return (
